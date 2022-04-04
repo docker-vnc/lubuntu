90d390f26e35b4a1cf58499df7c1e84493d9e01a
Sugeng Tigefa <tigefa@gmail.com>
2022-04-04 22:16:33 +0700

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -3,4 +3,4 @@
 # Remove VNC lock (if process already killed)
 rm -rf /tmp/.X1-lock /tmp/.X11-unix/X1
 # Run VNC server with tail in the foreground
-vncserver :1 -geometry 1346x740 && tail -F ~/.vnc/*.log
+vncserver :1 -geometry 1346x940 && tail -F ~/.vnc/*.log
